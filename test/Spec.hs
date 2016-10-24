79b3e4833d70e05c964bba31e0ba0ab92ab77e6b
joe9 <joe9mail@gmail.com>
2016-10-24 13:41:05 -0700

    added tests

---
+++ test/Spec.hs
@@ -0,0 +1,15 @@
+
+module Main where
+
+import           Test.Tasty (defaultMain, testGroup)
+--
+import           Data.NineP
+--
+import qualified NineP.Tests
+
+-- got this idea from
+--  https://jaspervdj.be/posts/2015-03-13-practical-testing-in-haskell.html
+main :: IO ()
+main = defaultMain $ testGroup "Tests"
+    [ NineP.Tests.tests
+    ]
